5cc09ae9f054b1972459c56acd435da846e59b70
ThomasTheTank28 <connor.reddington@mtbethelchristian.org>
2023-09-11 17:27:32 -0400

    LONG LIVE TACO

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Connor/BNI_Testing/TEST_RoadrunAutoCenterStage_CWR.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.BNI_Team.Connor.BNI_Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous (name = "ROADRUNAuto_CWR")
+public abstract class TEST_RoadrunAutoCenterStage_CWR extends LinearOpMode {
+
+    public static double ANGLE1 = 90;
+}
