2a62a2ccb52bcf83a80fe41c01677ae63ff27d49
omtCoder <olivia.tindall@mtbethelchristian.org>
2023-09-14 17:12:43 -0400

    Sensei Reddington is smart.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Olivia/MecanumTeleOp_Olivia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Olivia/MecanumTeleOp_Olivia.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp
+@TeleOp (name = "CONNOR IS THE BEST_MecanumTeleopTindall")
 public class MecanumTeleOp_Olivia extends OpMode {
 
     double leftStickYVal;
