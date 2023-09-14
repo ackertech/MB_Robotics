ea049212c9a3655e0b3233b1a5734937d7df19cd
ThomasTheTank28 <connor.reddington@mtbethelchristian.org>
2023-09-14 13:59:04 -0400

    LONG LIVE TACO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Connor/BNI_Testing/Test_AUTOMAINCenterStage_Connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Connor/BNI_Testing/Test_AUTOMAINCenterStage_Connor.java
@@ -1,15 +1,23 @@
 package org.firstinspires.ftc.teamcode.BNI_Team.Connor.BNI_Testing;
 
+import org.openftc.apriltag.AprilTagDetection;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Test_AUTOMAINCenterStage_Connor extends TEST_AprilTagNewCenterStage_CWR {
+int ID_TAG_LIST [] = {0,1,2,3,4,5,6,7};
+public AprilTagDetection tagOfInterest = null;
 
 
 
-    public void findTag(){
+    public void findTag() {
         initAprilTag();
         List<org.firstinspires.ftc.vision.apriltag.AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+
         telemetry.addData("# AprilTags Detected", currentDetections.size());
+
 
     }
 
@@ -26,7 +34,7 @@ public abstract class Test_AUTOMAINCenterStage_Connor extends TEST_AprilTagNewCe
 
         sleep(500);
 
-        visionPortal.stopStreaming();
+        visionPortal.close();
 
         sleep(500);
     }
