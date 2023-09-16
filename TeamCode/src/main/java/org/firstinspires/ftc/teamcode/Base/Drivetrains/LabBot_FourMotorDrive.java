3e5262f60e2e2e5def68f58c1149261441dec61f
AckerTech <jamie.acker@mtbethelchristian.org>
2023-09-15 22:28:49 -0400

    8th Grade Directories Added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Drivetrains/LabBot_FourMotorDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Drivetrains/LabBot_FourMotorDrive.java
@@ -12,8 +12,6 @@ public class LabBot_FourMotorDrive {
     public DcMotor rearLeftMotor;
     public DcMotor rearRightMotor;
 
-    //Tracking elapsed runtime
-    public ElapsedTime runtime = new ElapsedTime();
 
     // This is just required as part of the FIRST SDK.  Memorize it!!!
     public LinearOpMode linearOp = null;
@@ -26,6 +24,54 @@ public class LabBot_FourMotorDrive {
    public final DcMotor.RunMode currentMotorRunMode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
     public static final double TICKS_PER_ROTATION = 538;
 
+    // Stops the Motors
+    public void stopMotors () {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        rearLeftMotor.setPower(0);
+        rearRightMotor.setPower(0);
+    }
+
+
+    // Drivetrain Behaviors (aka Movement)
+
+    // Drives Forward Method
+    public void driveForward (double power) {
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(power);
+        rearLeftMotor.setPower(power);
+        rearRightMotor.setPower(power);
+    }
+
+    // Drives Backwards Method
+    public void driveBackward (double power) {
+
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(-power);
+        rearLeftMotor.setPower(-power);
+        rearRightMotor.setPower(-power);
+    }
+
+    //Rotates Left
+    public void rotateLeft (double power) {
+
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(power);
+        rearLeftMotor.setPower(-power);
+        rearRightMotor.setPower(power);
+
+    }
+
+    //Rotates Right
+    public void rotateRight (double power) {
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(-power);
+        rearLeftMotor.setPower(power);
+        rearRightMotor.setPower(-power);
+    }
+
     //Common method that can be used in our TeleOp and Autonomous programs
     public void setMotorRunModes (DcMotor.RunMode mode) {
 
@@ -35,56 +81,6 @@ public class LabBot_FourMotorDrive {
         rearRightMotor.setMode(mode);
     }
 
-    // Movement methods
-
-    // Stops the Motors
-    public void stopMotors () {
-        frontLeftMotor.setPower(0);
-        frontRightMotor.setPower(0);
-        rearLeftMotor.setPower(0);
-        rearRightMotor.setPower(0);
-    }
-
-    // Drives Forward
-    public void driveForward (double power) {
-
-        double ABSpower = Math.abs(power);
-        frontLeftMotor.setPower(ABSpower);
-        frontRightMotor.setPower(ABSpower);
-        rearLeftMotor.setPower(ABSpower);
-        rearRightMotor.setPower(ABSpower);
-    }
-
-    // Drives Backwards
-    public void driveBackward (double power) {
-
-        double ABSpower = Math.abs(power);
-        frontLeftMotor.setPower(-ABSpower);
-        frontRightMotor.setPower(-ABSpower);
-        rearLeftMotor.setPower(-ABSpower);
-        rearRightMotor.setPower(-ABSpower);
-    }
-
-    //Rotates Left
-    public void rotateLeft (double power) {
-
-        double ABSpower = Math.abs(power);
-        frontLeftMotor.setPower(-ABSpower);
-        frontRightMotor.setPower(ABSpower);
-        rearLeftMotor.setPower(-ABSpower);
-        rearRightMotor.setPower(ABSpower);
-
-    }
-
-    //Rotates Right
-    public void rotateRight (double power) {
-
-        double ABSpower = Math.abs(power);
-        frontLeftMotor.setPower(ABSpower);
-        frontRightMotor.setPower(-ABSpower);
-        rearLeftMotor.setPower(ABSpower);
-        rearRightMotor.setPower(-ABSpower);
-    }
 
     //******Drive without Encoder Methods********
     public void driveForward( double power, double rotations) {
@@ -142,67 +138,15 @@ public class LabBot_FourMotorDrive {
         stopMotors();
     }
 
-    //******Drive with Encoder Methods********
-
-    public void driveForward( double power, double rotations, double timeout) {
-
-        double ticks = Math.abs(rotations) * TICKS_PER_ROTATION;
-        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setMotorRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
-        runtime.reset();
-
-        while (frontLeftMotor.getCurrentPosition() < ticks && linearOp.opModeIsActive() && runtime.seconds() < timeout) {
-            driveForward(power);
-            linearOp.telemetry.addData("Ticks: ", frontLeftMotor.getCurrentPosition());
-            linearOp.telemetry.addData("RunTime: ", runtime);
-            linearOp.telemetry.update();
-        }
-        stopMotors();
-    }
-
-    public void driveBackward ( double power, double rotations,double timeout){
-
-        double ticks = rotations * (-1) * TICKS_PER_ROTATION;
-        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setMotorRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        while (frontLeftMotor.getCurrentPosition() > ticks && linearOp.opModeIsActive() && runtime.seconds() < timeout) {
-            driveBackward(power);
-            linearOp.telemetry.addData("Ticks: ", frontLeftMotor.getCurrentPosition());
-            linearOp.telemetry.addData("RunTime: ", runtime);
-            linearOp.telemetry.update();
-        }
-        stopMotors();
-    }
-
-    public void rotateLeft (double power, double rotations,double timeout) {
-        double ticks = Math.abs(rotations) * (-1) * TICKS_PER_ROTATION; //strafing left moves encoder towards positive infinity
-        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setMotorRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        while (frontLeftMotor.getCurrentPosition() > ticks && linearOp.opModeIsActive() && runtime.seconds() < timeout) {
-            rotateLeft(power);
-            linearOp.telemetry.addData("Ticks: ", frontLeftMotor.getCurrentPosition());
-            linearOp.telemetry.addData("RunTime: ", runtime);
-            linearOp.telemetry.update();
-        }
-        stopMotors();
-    }
 
 
-    public void rotateRight (double power, double rotations,double timeout) {
-        double ticks = Math.abs(rotations) * TICKS_PER_ROTATION; //strafing right moves encoder towards -infinity
-        setMotorRunModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setMotorRunModes(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        while(frontLeftMotor.getCurrentPosition() < ticks && linearOp.opModeIsActive() && runtime.seconds() < timeout) {
-            rotateRight(power);
-            linearOp.telemetry.addData("Ticks: ", frontLeftMotor.getCurrentPosition());
-            linearOp.telemetry.addData("RunTime: ", runtime);
-            linearOp.telemetry.update();
-        }
-        stopMotors();
-    }
+
+
+
+
+
+
 
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Drivetrains/LabBot_FourMotorDrive_Basic.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.Base.Drivetrains;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class LabBot_FourMotorDrive_Basic {
+
+    // Declared Variables for our Motors
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor rearLeftMotor;
+    public DcMotor rearRightMotor;
+
+    // This is just required as part of the FIRST SDK.
+    public LinearOpMode linearOp = null;
+
+
+    // Stops the Motors
+    public void stopMotors () {
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        rearLeftMotor.setPower(0);
+        rearRightMotor.setPower(0);
+    }
+
+    // Drives Forward Method
+    public void driveForward (double power) {
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(power);
+        rearLeftMotor.setPower(power);
+        rearRightMotor.setPower(power);
+    }
+
+    // Drives Backwards Method
+    public void driveBackward (double power) {
+
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(-power);
+        rearLeftMotor.setPower(-power);
+        rearRightMotor.setPower(-power);
+    }
+
+    //Rotates Left
+    public void rotateLeft (double power) {
+
+        frontLeftMotor.setPower(-power);
+        frontRightMotor.setPower(power);
+        rearLeftMotor.setPower(-power);
+        rearRightMotor.setPower(power);
+    }
+
+    //Rotates Right
+    public void rotateRight (double power) {
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(-power);
+        rearLeftMotor.setPower(power);
+        rearRightMotor.setPower(-power);
+    }
+
+    //Common method that can be used in our TeleOp and Autonomous programs
+    public void setMotorRunModes (DcMotor.RunMode mode) {
+
+        frontLeftMotor.setMode(mode);
+        frontRightMotor.setMode(mode);
+        rearLeftMotor.setMode(mode);
+        rearRightMotor.setMode(mode);
+    }
+
+    // This is just required as part of the FIRST SDK.
+    public void setLinearOp (LinearOpMode linearOpModeOp) {
+
+        this.linearOp = linearOp;
+    }
+
+}
