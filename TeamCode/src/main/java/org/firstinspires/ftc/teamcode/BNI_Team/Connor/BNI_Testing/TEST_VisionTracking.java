f44b7d13b8b56afedb84ccd7cf13aec7ef019241
ThomasTheTank28 <connor.reddington@mtbethelchristian.org>
2023-10-07 09:07:11 -0400

    LONG LIVE TACO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Connor/BNI_Testing/TEST_VisionTracking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BNI_Team/Connor/BNI_Testing/TEST_VisionTracking.java
@@ -1,80 +1,69 @@
 package org.firstinspires.ftc.teamcode.BNI_Team.Connor.BNI_Testing;
 
+import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-@TeleOp(name = "TEST_Vision Tracking Center Stage")
+
+
+
+
+
+
+@TeleOp (name = "TEST_HuskyLens_Vision Tracking Center Stage")
 public class TEST_VisionTracking extends LinearOpMode {
 
+    public final int READ_PERIOD = 1;
 
-    public static final boolean USE_WEBCAM = true;
-
-    public TfodProcessor tFod;
-
-    public VisionPortal visionPortal;
-
+    public HuskyLens huskyLens;
 
     @Override
     public void runOpMode(){
+        huskyLens = hardwareMap.get(HuskyLens.class,"huskylens");
 
-        initTfod();
+        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
 
-        waitForStart();
+        rateLimit.expire();
 
-        if (opModeIsActive()) {
-            while ( opModeIsActive()) {}
 
-            telemetrytFod();
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        }
+        else {
+            telemetry.addData(">>", "Press start to continue");
+        }
 
-            telemetry.update();
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.OBJECT_RECOGNITION);
 
-            if (gamepad1.dpad_down) {
-                visionPortal.stopStreaming();
-            } else if (gamepad1.dpad_up) {
-                visionPortal.resumeStreaming();
+        while (opModeIsActive()) {
+            if (!rateLimit.hasExpired()) {
+                continue;
             }
+
+            rateLimit.reset();
+
+
+
+
+            HuskyLens.Block[] blocks = huskyLens.blocks();
+            telemetry.addData("Block count", blocks.length);
+            for (int i = 0; i < blocks.length; i++) {
+                telemetry.addData("Block", blocks[i].toString());
+            }
+            telemetry.update();
         }
     }
 
 
-    public void initTfod() {
 
-
-        tFod = TfodProcessor.easyCreateWithDefaults();
-
-
-        if (USE_WEBCAM) {
-            visionPortal = VisionPortal.easyCreateWithDefaults(
-                    hardwareMap.get(WebcamName.class, "Webcam 1"), tFod);
-        } else {
-            visionPortal = VisionPortal.easyCreateWithDefaults(
-                    BuiltinCameraDirection.BACK, tFod);
-        }
-
-    }
-
-    public void telemetrytFod(){
-        List<Recognition> currentRecognitions = tFod.getRecognitions();
-        telemetry.addData("# Objects Detected", currentRecognitions.size());
-
-
-        for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-
-            telemetry.addData(""," ");
-            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-        }
-    }
-}
-
+}   // end class
