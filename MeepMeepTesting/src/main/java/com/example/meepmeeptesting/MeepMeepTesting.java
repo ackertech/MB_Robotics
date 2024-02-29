ad413aa83e6f063c4ab260463dcfacd03a3be486
connor.reddington <connor.reddington@mtbethelchristian.org>
2024-02-29 12:57:43 -0500

    LONG LIVE TACO

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -124,29 +124,68 @@ public class MeepMeepTesting {
 
 
                                     //blue audience new
-                            drive.trajectorySequenceBuilder(new Pose2d(-35, 58, 4.7))
-                                    .lineToLinearHeading(new Pose2d(-36,31,Math.toRadians(182)))
-                                    .lineTo(new Vector2d(-36,20))
-                                    .splineToConstantHeading(new Vector2d(-52,11),Math.toRadians(-180))
-                                    .lineTo(new Vector2d(37,11))
-                                    .lineToLinearHeading(new Pose2d(45,31,Math.toRadians(1)))
-                                    .lineToLinearHeading(new Pose2d(37,11,Math.toRadians(180 )))
-                                    .lineTo(new Vector2d(-52,11))
-                                    .lineTo(new Vector2d(37,11))
-                                    .lineToLinearHeading(new Pose2d(45,31,Math.toRadians(1)))
-                                    .lineToLinearHeading(new Pose2d(45,5,Math.toRadians(45)))
-                                    .forward(5)
+//                            drive.trajectorySequenceBuilder(new Pose2d(-35, 58, 4.7))
+//                                    .lineToLinearHeading(new Pose2d(-36,31,Math.toRadians(182)))
+//                                    .lineTo(new Vector2d(-36,20))
+//                                    .splineToConstantHeading(new Vector2d(-52,11),Math.toRadians(-180))
+//                                    .lineTo(new Vector2d(37,11))
+//                                    .lineToLinearHeading(new Pose2d(45,31,Math.toRadians(1)))
+//                                    .lineToLinearHeading(new Pose2d(37,11,Math.toRadians(180 )))
+//                                    .lineTo(new Vector2d(-52,11))
+//                                    .lineTo(new Vector2d(37,11))
+//                                    .lineToLinearHeading(new Pose2d(45,31,Math.toRadians(1)))
+//                                    .lineToLinearHeading(new Pose2d(45,5,Math.toRadians(45)))
+//                                    .forward(5)
 //
 
 
 
 
 //                            //RED BACKSTAGE SIMPLE (For Acker)
-//                                    drive.trajectorySequenceBuilder(new Pose2d(13, -58, -4.7))
-//                                            .lineToLinearHeading(new Pose2d(13,-31,Math.toRadians(182)))
-//                                             .lineToLinearHeading(new Pose2d(45,-31,Math.toRadians(1)))
-//                                            .lineToLinearHeading(new Pose2d(45,-10,Math.toRadians(-45)))
-//                                            .forward(5)
+//                                    drive.trajectorySequenceBuilder(new Pose2d(13, -60, -4.7))
+//                                            .forward(27)
+//                                            .turn(Math.toRadians(90))
+//                                            .back(30)
+//                                            .turn(Math.toRadians(-180))
+//                                            .strafeLeft(22)
+//                                            .back(90)
+//                                            .turn(Math.toRadians(180))
+//                                            .back(90)
+//                                            .turn(Math.toRadians(180))
+//                                            .strafeRight(22)
+//                                            .strafeRight(27)
+//                                            .forward(17)
+//
+//                                            .forward(15)
+
+                                            //blue backstage simple
+//                            drive.trajectorySequenceBuilder(new Pose2d(13, 58, 4.7))
+//                                    .forward(27)
+//                                    .strafeLeft(2)
+//                                    .turn(Math.toRadians(-90))
+//                                    .forward(1)
+//                                    .back(29)
+//                                    .turn(Math.toRadians(180))
+//                                    .strafeLeft(7)
+//                                    .strafeRight(27)
+//                                    .back(90)
+//                                    .turn(Math.toRadians(-180))
+//                                    .back(90)
+//                                    .turn(Math.toRadians(-180))
+//                                    .strafeLeft(22)
+//                                    .strafeLeft(27)
+//                                    .forward(17)
+
+
+                                    //red audience simple
+                            drive.trajectorySequenceBuilder(new Pose2d(-35, -57, -4.7))
+                                    .forward(24)
+                                    .turn(Math.toRadians(-90))
+                                    .turn(Math.toRadians(90))
+                                    .forward(24)
+                                    .turn(Math.toRadians(-90))
+                                    .forward(80)
+                                    .strafeRight(30)
 
 
 
@@ -180,7 +219,7 @@ public class MeepMeepTesting {
 
                     );
 
-            meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+            meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
                     .setDarkMode(true)
                     .setBackgroundAlpha(0.95f)
                     .addEntity(myBot)
