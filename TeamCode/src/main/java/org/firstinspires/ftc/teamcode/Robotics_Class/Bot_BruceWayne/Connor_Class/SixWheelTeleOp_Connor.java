8b789b992cbe2a08378d614ecc5c68678435fed9
connor.reddington <connor.reddington@mtbethelchristian.org>
2024-03-19 12:49:16 -0400

    LONG LIVE TACO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_Class/Bot_BruceWayne/Connor_Class/SixWheelTeleOp_Connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_Class/Bot_BruceWayne/Connor_Class/SixWheelTeleOp_Connor.java
@@ -79,6 +79,7 @@ public class SixWheelTeleOp_Connor extends OpMode {
         speedControl();
         drivingMode();
         intakeControl();
+        launcher();
         //  candyLauncher();
         // lazySusan();
         linearActuator();
