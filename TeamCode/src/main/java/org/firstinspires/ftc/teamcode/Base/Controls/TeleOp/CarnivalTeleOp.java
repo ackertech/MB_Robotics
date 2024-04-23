dea7e75bdbb6f22358af01700e3f21a6bade7737
AckerTech <jamie.acker@mtbethelchristian.org>
2024-04-23 09:13:52 -0400

    Canival Madness Robot Template Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/CarnivalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base/Controls/TeleOp/CarnivalTeleOp.java
@@ -11,14 +11,25 @@ import org.firstinspires.ftc.teamcode.Base.Robot.CarnivalBot;
 @Disabled
 @TeleOp (name = "Carnival Bot")
 public class CarnivalTeleOp extends OpMode {
+
+    // Drive Control Instance variables
     double leftStickYVal;
+    double leftStickXVal;
     double rightStickYVal;
+    double rightStickXVal;
     double speedMultiply = 1.0;
     public double leftSidePower;
     public double rightSidePower;
 
+    // Driver Style Control Instance variables
+    public boolean arcade2StickDrive = false;
+    public boolean arcade1StickDrive = false;
+    public boolean tankDrive = true;
+
+    // Mechanism Speed variables
     public double wormGearPower = 0.90;
 
+    // Constructor
     public CarnivalBot bot = new CarnivalBot();
 
 
@@ -36,7 +47,8 @@ public class CarnivalTeleOp extends OpMode {
     @Override
     public void loop() {
         speedControl();
-        driveTankControl();
+        driverStyleControl();
+        driveControl();
         flyWheelControl();
         wormGearControl();
         linearActuatorControl();
@@ -66,18 +78,6 @@ public class CarnivalTeleOp extends OpMode {
 
     }
 
-    public void driveTankControl() {
-        leftStickYVal = gamepad1.left_stick_y;
-        leftStickYVal = Range.clip(leftStickYVal, -1, 1);
-
-        rightStickYVal = gamepad1.right_stick_y;
-        rightStickYVal = Range.clip(rightStickYVal, -1, 1);
-
-        leftSidePower = speedMultiply * leftStickYVal * (1);
-        rightSidePower = speedMultiply * rightStickYVal * (1);
-        bot.tankDrive(leftSidePower, rightSidePower);
-
-    }
 
     public void balloonPopperControl() {
         if (gamepad1.left_bumper) {
@@ -89,6 +89,82 @@ public class CarnivalTeleOp extends OpMode {
         }
 
     }
+
+    public void driverStyleControl() {
+        if (gamepad1.a) {
+            arcade2StickDrive = false;
+            arcade1StickDrive = false;
+            tankDrive = true;
+        }
+
+        if (gamepad1.b) {
+            arcade2StickDrive = true;
+            arcade1StickDrive = false;
+            tankDrive = false;
+        }
+
+        if (gamepad1.y) {
+            arcade2StickDrive = false;
+            arcade1StickDrive = true;
+            tankDrive = false;
+        }
+
+    }
+
+    public void driveControl() {
+
+        leftStickYVal = gamepad1.left_stick_y;
+        leftStickYVal = Range.clip(leftStickYVal, -1, 1);
+
+        leftStickXVal = gamepad1.left_stick_x;
+        leftStickXVal = Range.clip(leftStickXVal, -1, 1);
+
+        rightStickYVal = gamepad1.right_stick_y;
+        rightStickYVal = Range.clip(rightStickYVal, -1, 1);
+
+        rightStickXVal = gamepad1.right_stick_x;
+        rightStickXVal = Range.clip(rightStickXVal, -1, 1);
+
+
+        if (arcade1StickDrive) {
+
+            if (gamepad1.left_stick_y < -0.1) {
+                bot.driveForward(speedMultiply * leftStickYVal);
+            } else if (gamepad1.left_stick_y > 0.1) {
+                bot.driveBack(speedMultiply * leftStickYVal);
+            } else if (gamepad1.left_stick_x > 0.1) {
+                bot.rotateRight(speedMultiply * leftStickXVal);
+            } else if (gamepad1.left_stick_x < -0.1) {
+                bot.rotateLeft(speedMultiply * leftStickXVal);
+            } else {
+                bot.stopMotors();
+            }
+        }
+        else if (arcade2StickDrive) {
+
+            if (gamepad1.left_stick_y < -0.1) {
+                bot.driveForward(speedMultiply * leftStickYVal);
+            } else if (gamepad1.left_stick_y > 0.1) {
+                bot.driveBack(speedMultiply * leftStickYVal);
+            } else if (gamepad1.right_stick_x > 0.1) {
+                bot.rotateRight(speedMultiply * rightStickXVal);
+            } else if (gamepad1.right_stick_x < -0.1) {
+                bot.rotateLeft(speedMultiply * rightStickXVal);
+            } else {
+                bot.stopMotors();
+            }
+
+        }
+        else if (tankDrive) {
+
+            leftSidePower = speedMultiply * leftStickYVal * (1);
+            rightSidePower = speedMultiply * rightStickYVal * (1);
+            bot.tankDrive(leftSidePower, rightSidePower);
+
+        }
+
+    }
+
 
     // ********  Driver Two Controls on GamePad 2  **************
 
